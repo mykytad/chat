e7ea0a334378ebbc36f31e327d08f09a09cc1169
Mykyta Dynnik <mykytadynnik@gmail.com>
2024-02-03 16:25:28 +0200

    add static page controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   
+  get "about" => "pages#about"
+  get "contacts" => "pages#contacts"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
