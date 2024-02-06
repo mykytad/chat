4c219f481bcf90db3aa5e7c046c30b7d3688cc31
Mykyta Dynnik <mykytadynnik@gmail.com>
2024-02-06 17:02:44 +0200

    add devise views

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,15 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
   end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new
+    if @user.save
+      @user.nick_name = "#" + @user.id.to_s
+    end
+  end
 end
