492b07729f104f147231cab30734f4cbb4d87c8a
Mykyta Dynnik <mykytadynnik@gmail.com>
2024-02-07 13:38:07 +0200

    change shema add user view

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,6 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:phone, :name, :nick_name])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:phone, :name])
   end
 end
