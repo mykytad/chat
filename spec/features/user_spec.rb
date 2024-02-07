e72ec47b63a92a7cf0bd86d4fa7f8e24f3f2f3d5
Mykyta Dynnik <mykytadynnik@gmail.com>
2024-02-07 14:56:06 +0200

    gem

---
+++ spec/features/user_spec.rb
@@ -0,0 +1,24 @@
+require "rails_helper"
+# require "shared_methods"
+
+RSpec.feature "create user", type: :feature do
+  it "creates a new user" do
+    visit new_user_registration_path
+    fill_in :user_name, with: "Tom"
+    fill_in :user_phone, with: "0987654321"
+    fill_in :user_email, with: "tom@example.com"
+    fill_in :user_password, with: "111111"
+    fill_in :user_password_confirmation, with: "111111"
+    click_button "Sign up"
+
+    expect(body).to have_content "Tom"
+    expect(body).to have_link "Sign out"
+  end
+
+  it "profile show" do
+    log_in(test_user)
+    click_link :profile_link
+
+    expect(body).to have_link "Tomas"
+  end
+end
