c03f238fd71ffffa0a6d2675c490eb3ef1f033a9
Mykyta Dynnik <mykytadynnik@gmail.com>
2024-04-02 13:46:52 +0300

    modified workflows, messages views & seed

--- app/models/message.rb
+++ app/models/message.rb
@@ -5,4 +5,6 @@ class Message < ApplicationRecord
   validates :body, presence: true
   validates :dialogue_id, presence: true
   validates :user_id, presence: true
+
+  # after_create_commit { broadcast_prepend_to "messages", partial: "messages/new", locals: { messages: self }, target: "messages" }
 end
