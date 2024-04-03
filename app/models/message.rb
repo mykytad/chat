a44409f0312b23cf69bccfebea976596383db691
Mykyta Dynnik <mykytadynnik@gmail.com>
2024-04-03 20:18:14 +0300

    -

--- app/models/message.rb
+++ app/models/message.rb
@@ -6,6 +6,6 @@ class Message < ApplicationRecord
   validates :dialogue_id, presence: true
   validates :user_id, presence: true
 
-  # after_create_commit { broadcast_prepend_to "messages", partial: "messages/new", locals: { messages: self }, target: "messages" }
-  # after_create_commit { broadcast_append_to self.room }
+  # after_create_commit -> { broadcast_prepend_to "messages", partial: "messages/message", locals: { message: self }, target: "messages" }
+  # after_create_commit { broadcast_append_to self.dialogue }
 end
