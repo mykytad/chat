13c001fccfe3fda27c84b9f5fd4a4dc16718b7e8
Mykyta Dynnik <mykytadynnik@gmail.com>
2024-04-16 15:29:41 +0300

    add new version after create commit

--- app/models/message.rb
+++ app/models/message.rb
@@ -6,6 +6,7 @@ class Message < ApplicationRecord
   validates :dialogue_id, presence: true
   validates :user_id, presence: true
 
+  # after_create_commit -> { broadcast_prepend_to "messages" }
   # after_create_commit -> { broadcast_prepend_to "messages", partial: "messages/message", locals: { message: self }, target: "messages" }
   # after_create_commit { broadcast_append_to self.dialogue }
 end
