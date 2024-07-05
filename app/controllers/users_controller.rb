f931abf837f55d49a920926bd99b806272d6a126
Mykyta Dynnik <mykytadynnik@gmail.com>
2024-07-05 16:29:22 +0300

    add & configured avatarly, add avatarly to user show

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,15 @@
 class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
-  end
+    @avatar = Digest::SHA1.hexdigest(@user.id.to_s) # хэш имени аватара
+    file_path = "public/images/#{@avatar}.png" # путь к файлу аватара
+    file = File.exists?(file_path)
 
-  def new
-    @user = User.new
+    if file == false
+      img = Avatarly.generate_avatar(@user.name, opts = { size: 40 })
+      File.open(file_path, 'wb') do |f|
+        f.write(img)
+      end
+    end
   end
 end
