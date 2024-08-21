30cc5f304a073ec4e4f50bf40128681796826b3a
Mykyta Dynnik <mykytadynnik@gmail.com>
2024-08-21 16:46:26 +0300

    -

--- db/migrate/20240711094322_create_pin_dialogues.rb
+++
@@ -1,9 +0,0 @@
-class CreatePinDialogues < ActiveRecord::Migration[7.0]
-  def change
-    create_table :pin_dialogues do |t|
-      t.integer :user_id,          null: false, default: ""
-      t.integer :dialogue_id,      null: false, default: ""
-      t.timestamps
-    end
-  end
-end
