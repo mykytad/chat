fd681fbe5ce392414c8b4a3eea768bf8778de0c8
Mykyta Dynnik <mykytadynnik@gmail.com>
2024-08-23 14:12:01 +0300

    adede new controller methods

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,5 +1,7 @@
 class MessagesController < ApplicationController
   before_action :dialogue
+  before_action :user_dialogues
+  before_action :recipient
 
   def index
     @dialogues = Dialogue.all.order(:updated_at => :DESC)
@@ -48,5 +50,25 @@ class MessagesController < ApplicationController
 
   def dialogue
     @dialogue ||= Dialogue.find(params[:dialogue_id])
+  end
+
+  def user_dialogues
+    @dialogues = Dialogue.all
+    @user_dialogues =[]
+    @dialogues.each do |dialogue|
+      if dialogue.sender_id == current_user.id || dialogue.recipient_id == current_user.id
+        @user_dialogues << dialogue
+      end
+    end
+  end
+
+  def recipient
+    @user_dialogues.each do |dialogue|
+      if dialogue.sender_id == current_user.id
+        @recipient = User.find(dialogue.recipient_id)
+      else
+        @recipient = User.find(dialogue.sender_id)
+      end
+    end
   end
 end
