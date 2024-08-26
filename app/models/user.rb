a7b03701b1bbee9b7090f840985dc35d57b6c6a5
Mykyta Dynnik <mykytadynnik@gmail.com>
2024-08-26 16:28:16 +0300

    fix rubocop

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   has_many :messages
   has_many :dialogues
-  
+
   # validates :nickname, uniqueness: true
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
