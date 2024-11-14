26ee05a3d6cb36b85e1b2a8f0d50330cb23fff4f
Mykyta Dynnik <mykytadynnik@gmail.com>
2024-11-14 14:10:24 +0200

    added dialogue channel js

--- app/javascript/channels/dialogue_channel.js
+++ app/javascript/channels/dialogue_channel.js
@@ -2,11 +2,14 @@ import consumer from "./consumer"
 
 consumer.subscriptions.create("DialogueChannel", {
   connected() {
-  },
-
-  disconnected() {
+    console.log("connected to DialogueChannel");
   },
 
   received(data) {
+    console.log("received data from DialogueChannel:", data);
+  },
+
+  disconnected() {
+    console.log("disconnected from DialogueChannel");
   }
 })
