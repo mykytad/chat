44795fe35975b4769e1d717b0524c17b43ec4b2c
Mykyta Dynnik <mykytadynnik@gmail.com>
2024-11-26 15:35:39 +0200

    fix dialogue channal

--- app/channels/dialogue_channel.rb
+++ app/channels/dialogue_channel.rb
@@ -1,18 +1,7 @@
 class DialogueChannel < ApplicationCable::Channel
   def subscribed
-    # stream_from "some_channel"
-
-    # Find the dialogue based on the dialogue_id parameter
-    # dialogue = Dialogue.find(params[:id])
-    # messages = dialogue.messages
-    # Stream for the found dialogue
-    # stream_for messages
-    # rescue ActiveRecord::RecordNotFound
-    # If the chat is not found, reject the subscription
-    # reject
   end
 
   def unsubscribed
-    # Any cleanup needed when channel is unsubscribed
   end
 end
