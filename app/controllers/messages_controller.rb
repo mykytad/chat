3aa8f23ab864b5b4512f90d5721909a3af1a8b97
Mykyta Dynnik <mykytadynnik@gmail.com>
2024-12-02 16:37:48 +0200

    added test for read method

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -54,9 +54,12 @@ class MessagesController < ApplicationController
 
   def destroy
     @message = dialogue.messages.find(params[:id])
+  
     if @dialogue.sender_id == current_user.id || @dialogue.recipient_id == current_user.id
       @message.destroy
       redirect_to dialogue_messages_path(@dialogue)
+    else
+      redirect_to dialogues_path, alert: "you can't delete this message"
     end
   end
 
