9891882bb8c68eb2ba6bc353b750105ce847e0b2
Mykyta Dynnik <mykytadynnik@gmail.com>
2024-12-04 17:15:10 +0200

    improved sidebar js

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -4,3 +4,5 @@ import "@hotwired/turbo-rails"
 import "channels"
 import "./controllers";
 import "./message_highlight";
+import "./sidebar_message";
+import "./sidebar_dialogue";
--- app/javascript/sidebar_message.js
+++ app/javascript/sidebar_message.js
@@ -1,26 +1,28 @@
-function showSidebar() {
-  var backArrow = document.getElementById('back-arrow');
-  var sidebar = document.querySelector('.sidebar');
-  var sidebarCross = document.getElementById('sidebar-cross');
-  var msgDisplay = document.querySelector('.msg-display');
+document.addEventListener("turbo:load", () => {
+  function showSidebar() {
+    var backArrow = document.getElementById('back-arrow');
+    var sidebar = document.querySelector('.sidebar');
+    var sidebarCross = document.getElementById('sidebar-cross');
+    var msgDisplay = document.querySelector('.msg-display');
 
-  backArrow.classList.add('d-none');
-  msgDisplay.classList.add('d-none');
-  sidebar.classList.remove('d-none');
-  sidebarCross.classList.remove('d-none');
-}
+    backArrow.classList.add('d-none');
+    msgDisplay.classList.add('d-none');
+    sidebar.classList.remove('d-none');
+    sidebarCross.classList.remove('d-none');
+  }
 
-function hideSidebar() {
-  var backArrow = document.getElementById('back-arrow');
-  var sidebar = document.querySelector('.sidebar');
-  var sidebarCross = document.getElementById('sidebar-cross');
-  var msgDisplay = document.querySelector('.msg-display');
+  function hideSidebar() {
+    var backArrow = document.getElementById('back-arrow');
+    var sidebar = document.querySelector('.sidebar');
+    var sidebarCross = document.getElementById('sidebar-cross');
+    var msgDisplay = document.querySelector('.msg-display');
 
-  backArrow.classList.remove('d-none');
-  msgDisplay.classList.remove('d-none');
-  sidebar.classList.add('d-none');
-  sidebarCross.classList.add('d-none');
-}
+    backArrow.classList.remove('d-none');
+    msgDisplay.classList.remove('d-none');
+    sidebar.classList.add('d-none');
+    sidebarCross.classList.add('d-none');
+  }
 
-document.getElementById('back-arrow').addEventListener('click', showSidebar);
-document.getElementById('sidebar-cross').addEventListener('click', hideSidebar);
+  document.getElementById('back-arrow').addEventListener('click', showSidebar);
+  document.getElementById('sidebar-cross').addEventListener('click', hideSidebar);
+});
