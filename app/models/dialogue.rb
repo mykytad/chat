88bbdd6cdcab78afccd1f63e554f1aca6b7f4175
Mykyta Dynnik <mykytadynnik@gmail.com>
2025-01-19 16:49:39 +0200

    work with user view

--- app/models/dialogue.rb
+++ app/models/dialogue.rb
@@ -1,6 +1,6 @@
 class Dialogue < ApplicationRecord
-  belongs_to :sender, foreign_key: :sender_id, class_name: 'User'
-  belongs_to :recipient, foreign_key: :recipient_id, class_name: 'User'
+  belongs_to :sender, foreign_key: :sender_id, class_name: "User"
+  belongs_to :recipient, foreign_key: :recipient_id, class_name: "User"
   has_many :messages, dependent: :destroy
 
   validates_uniqueness_of :sender_id, scope: :recipient_id
@@ -8,9 +8,9 @@ class Dialogue < ApplicationRecord
   after_update_commit { broadcast_replace_to "dialogues" }
 
   scope :between, ->(sender_id, recipient_id) do
-    where("(dialogues.sender_id = ? AND dialogues.recipient_id =?) OR
+          where("(dialogues.sender_id = ? AND dialogues.recipient_id =?) OR
     (dialogues.sender_id = ? AND dialogues.recipient_id =?)", sender_id, recipient_id, recipient_id, sender_id)
-  end
+        end
 
   def unread_messages_count_for(user_id)
     messages.where(read: false).where.not(user_id: user_id).count
--- app/models/message.rb
+++ app/models/message.rb
@@ -1,9 +1,9 @@
 class Message < ApplicationRecord
   belongs_to :user
   belongs_to :dialogue
-  belongs_to :replied_to, class_name: 'Message', optional: true
+  belongs_to :replied_to, class_name: "Message", optional: true
 
-  has_many :replies, class_name: 'Message', foreign_key: 'replied_to_id'
+  has_many :replies, class_name: "Message", foreign_key: "replied_to_id"
 
   validates :body, presence: true
   validates :dialogue_id, presence: true
