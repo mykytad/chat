6769a8afb7e5e2d05722e6f264939f7af5ee1e93
Mykyta Dynnik <mykytadynnik@gmail.com>
2025-01-23 13:27:56 +0200

    added new migration

---
+++ db/migrate/20250123112151_add_images_to_message.rb
@@ -0,0 +1,6 @@
+class AddImagesToMessage < ActiveRecord::Migration[7.0]
+  def change
+    add_column :messages, :images, :string
+
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2025_01_07_104253) do
+ActiveRecord::Schema[7.0].define(version: 2025_01_23_112151) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -37,6 +37,7 @@ ActiveRecord::Schema[7.0].define(version: 2025_01_07_104253) do
     t.text "link_description"
     t.string "link_image"
     t.string "link_url"
+    t.string "images"
   end
 
   create_table "users", force: :cascade do |t|
