ffe12bb00a16c3a81301a691e2f2226781463962
Mykyta Dynnik <mykytadynnik@gmail.com>
2025-01-27 16:09:14 +0200

    configured image size

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -30,7 +30,7 @@ class ImageUploader < CarrierWave::Uploader::Base
 
   # Create different versions of your uploaded files:
   version :thumb do
-    process resize_to_fit: [250, 250]
+    process resize_to_fit: [500, 500]
   end
 
   # Add an allowlist of extensions which are allowed to be uploaded.
