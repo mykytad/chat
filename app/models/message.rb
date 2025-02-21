e9c921237de5775670bd42aaf4c74b3129f669de
Mykyta Dynnik <mykytadynnik@gmail.com>
2025-02-21 14:26:12 +0200

    modified message model

--- app/models/message.rb
+++ app/models/message.rb
@@ -18,7 +18,7 @@ class Message < ApplicationRecord
 
   after_create_commit { broadcast_append_to "dialogue_#{dialogue.id}_messages" }
   after_update_commit { broadcast_replace_to "dialogue_#{dialogue.id}_messages" }
-  after_destroy_commit { broadcast_remove_to "dialogue_#{dialogue.id}_messages" }
+  before_destroy { broadcast_remove_to "dialogue_#{dialogue.id}_messages" }
 
   private
 
