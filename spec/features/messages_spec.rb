cd9feb0473f8ee688c3d8e1d51a29f0c1571b166
Mykyta Dynnik <mykytadynnik@gmail.com>
2025-02-25 13:33:14 +0200

    fix spec

--- spec/features/messages_spec.rb
+++ spec/features/messages_spec.rb
@@ -14,7 +14,7 @@ RSpec.feature "messages pages", type: :feature do
     expect(body).to have_content "Foo"
 
     fill_in :message_body, with: "Hi, Foo"
-    within(".col-auto.col-3.col-lg-1") do
+    within(".col-auto.col-2.col-md-2.col-lg-1") do
       click_button "Send"
     end
 
